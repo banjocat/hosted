c24548f790f6963e2b94674cc0464419d3ab66ba
banjocat <jackmuratore@gmail.com>
2017-06-28 23:59:04 -0400

    Sticking with ansible for now

--- chef/knife.rb
+++
@@ -1,8 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                'banjocat'
-client_key               '/home/banjocat/code/fun/hosted/chef/banjocat.pem'
-validation_client_name   'chef-validator'
-validation_key           '/etc/chef-server/chef-validator.pem'
-chef_server_url          'http://localhost:8889'
-syntax_check_cache_path  '/home/banjocat/code/fun/hosted/chef/syntax_check_cache'
